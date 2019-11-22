797574f04f6389211db270d920c4c7bf2e5f12a6
Ovroogs <30376872+Ovroogs@users.noreply.github.com>
2019-11-22 16:10:48 +0300

    1
    1

---
+++ Lab13/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.названиеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.жителиDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.растояниеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.planetsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lab13DataSet = new Lab13.Lab13DataSet();
+            this.planetsTableAdapter = new Lab13.Lab13DataSetTableAdapters.planetsTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnCanel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Value = new System.Windows.Forms.TextBox();
+            this.Filter = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planetsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab13DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.названиеDataGridViewTextBoxColumn,
+            this.жителиDataGridViewTextBoxColumn,
+            this.растояниеDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.planetsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(470, 206);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // названиеDataGridViewTextBoxColumn
+            // 
+            this.названиеDataGridViewTextBoxColumn.DataPropertyName = "название";
+            this.названиеDataGridViewTextBoxColumn.HeaderText = "название";
+            this.названиеDataGridViewTextBoxColumn.Name = "названиеDataGridViewTextBoxColumn";
+            // 
+            // жителиDataGridViewTextBoxColumn
+            // 
+            this.жителиDataGridViewTextBoxColumn.DataPropertyName = "Жители";
+            this.жителиDataGridViewTextBoxColumn.HeaderText = "Жители";
+            this.жителиDataGridViewTextBoxColumn.Name = "жителиDataGridViewTextBoxColumn";
+            // 
+            // растояниеDataGridViewTextBoxColumn
+            // 
+            this.растояниеDataGridViewTextBoxColumn.DataPropertyName = "Растояние";
+            this.растояниеDataGridViewTextBoxColumn.HeaderText = "Растояние";
+            this.растояниеDataGridViewTextBoxColumn.Name = "растояниеDataGridViewTextBoxColumn";
+            // 
+            // planetsBindingSource
+            // 
+            this.planetsBindingSource.DataMember = "planets";
+            this.planetsBindingSource.DataSource = this.lab13DataSet;
+            // 
+            // lab13DataSet
+            // 
+            this.lab13DataSet.DataSetName = "Lab13DataSet";
+            this.lab13DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // planetsTableAdapter
+            // 
+            this.planetsTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(117, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Фильтрация данных";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(393, 299);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(89, 53);
+            this.btnFilter.TabIndex = 2;
+            this.btnFilter.Text = "Фильтрация";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnCanel
+            // 
+            this.btnCanel.Location = new System.Drawing.Point(505, 300);
+            this.btnCanel.Name = "btnCanel";
+            this.btnCanel.Size = new System.Drawing.Size(98, 52);
+            this.btnCanel.TabIndex = 3;
+            this.btnCanel.Text = "Отменить фильтр";
+            this.btnCanel.UseVisualStyleBackColor = true;
+            this.btnCanel.Click += new System.EventHandler(this.btnCanel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Фильтрация по полю";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Значение";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Value
+            // 
+            this.Value.Location = new System.Drawing.Point(198, 299);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(100, 20);
+            this.Value.TabIndex = 7;
+            // 
+            // Filter
+            // 
+            this.Filter.FormattingEnabled = true;
+            this.Filter.Location = new System.Drawing.Point(26, 299);
+            this.Filter.Name = "Filter";
+            this.Filter.Size = new System.Drawing.Size(121, 21);
+            this.Filter.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 364);
+            this.Controls.Add(this.Filter);
+            this.Controls.Add(this.Value);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCanel);
+            this.Controls.Add(this.btnFilter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.planetsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lab13DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Lab13DataSet lab13DataSet;
+        private System.Windows.Forms.BindingSource planetsBindingSource;
+        private Lab13DataSetTableAdapters.planetsTableAdapter planetsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn названиеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn жителиDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn растояниеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnCanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Value;
+        private System.Windows.Forms.ComboBox Filter;
+    }
+}
+
---
+++ Lab13/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.planetsTableAdapter.Fill(this.lab13DataSet.planets);
+            int ColumnCount = this.lab13DataSet.planets.Columns.Count;
+            for (int i = 0; i < ColumnCount; i++)
+                this.Filter.Items.Add(this.lab13DataSet.planets.Columns[i].ToString());
+            label4.Text = "";
+            Value.Text = "";
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {// установитьфильтр
+            string filter = this.Filter.Text;
+            this.planetsBindingSource.Filter = filter + " = '" + this.Value.Text + "'";
+            label4.Text = filter + " = '" + this.Value.Text + "'";
+        }
+
+        private void btnCanel_Click(object sender, EventArgs e)
+        {//отменитьфильтр
+            this.planetsBindingSource.Filter = "";
+            label4.Text = "";
+            Value.Text = "";
+        }
+    }
+
+}
